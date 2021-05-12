274626f49eb90bea10c174fb1a2a1f1c8c90bd05
Bastian Leicht <50463419+routerabfrage@users.noreply.github.com>
2021-05-12 11:24:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -140,9 +140,7 @@ Toolkit.run(
     );
 
     if (!content.length) {
-      tools.exit.success(
-        "No PullRequest/Issue/IssueComment events found. Leaving README unchanged with previous activity!"
-      );
+      tools.exit.success("No PullRequest/Issue/IssueComment events found. Leaving README unchanged with previous activity!");
     }
 
     if (content.length < 5) {
