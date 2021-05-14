6f67ce6da4cac3eb69169004f1b8ec93e612d7c4
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2021-05-14 17:57:27 +0200

    Fixed prettier

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const toUrlFormat = (item) => {
   if (typeof item === "object") {
     return Object.hasOwnProperty.call(item.payload, "issue")
       ? `[#${item.payload.issue.number}](${urlPrefix}/${item.repo.name}/issues/${item.payload.issue.number})`
-      : `[#${item.payload.pull_request.number}](${urlPrefix}/${item.repo.name}/pull/${item.payload.pull_request.number})`;   
+      : `[#${item.payload.pull_request.number}](${urlPrefix}/${item.repo.name}/pull/${item.payload.pull_request.number})`;
   }
   return `[${item}](${urlPrefix}/${item})`;
 };
