c920ff5fdb3f5dfe21f03be33823919bee661bfe
Alice <jacka.alice@gmail.com>
2021-08-17 17:44:15 +1000

    change INJECT to be path to js file

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ const processEvents = (events) => {
 
 Toolkit.run(
   async (tools) => {
-    eval(INJECT);
+    if (INJECT) require(INJECT);
 
     // Get the user's public events
     tools.log.debug(`Getting activity for ${GH_USERNAME}`);
