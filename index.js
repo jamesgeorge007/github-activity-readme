0cb0dd93ed90fd167c937290e99eb7fbfb0b05c0
Jan Larwig <jan@larwig.com>
2023-07-13 16:11:02 +0200

    bugfix: url generation handling of all types (#104)

--- index.js
+++ index.js
@@ -21,22 +21,30 @@ const TARGET_FILE = core.getInput("TARGET_FILE");
 
 const capitalize = (str) => str.slice(0, 1).toUpperCase() + str.slice(1);
 
-const urlPrefix = "https://github.com";
-
 /**
  * Returns a URL in markdown format for PR's and issues
  * @param {Object | String} item - holds information concerning the issue/PR
  *
  * @returns {String}
  */
-
 const toUrlFormat = (item) => {
-  if (typeof item === "object") {
-    return Object.hasOwnProperty.call(item.payload, "issue")
-      ? `[#${item.payload.issue.number}](${urlPrefix}/${item.repo.name}/issues/${item.payload.issue.number})`
-      : `[#${item.payload.pull_request.number}](${urlPrefix}/${item.repo.name}/pull/${item.payload.pull_request.number})`;
+  if (typeof item !== "object") {
+    return `[${item}](https://github.com/${item})`;
   }
-  return `[${item}](${urlPrefix}/${item})`;
+  if (Object.hasOwnProperty.call(item.payload, "comment")) {
+    return `[#${item.payload.issue.number}](${item.payload.comment.html_url})`;
+  }
+  if (Object.hasOwnProperty.call(item.payload, "issue")) {
+    return `[#${item.payload.issue.number}](${item.payload.issue.html_url})`;
+  }
+  if (Object.hasOwnProperty.call(item.payload, "pull_request")) {
+    return `[#${item.payload.pull_request.number}](${item.payload.pull_request.html_url})`;
+  }
+
+  if (Object.hasOwnProperty.call(item.payload, "release")) {
+    const release = item.payload.release.name || item.payload.release.tag_name;
+    return `[${release}](${item.payload.release.html_url})`;
+  }
 };
 
 /**
@@ -100,9 +108,7 @@ const serializers = {
   },
   ReleaseEvent: (item) => {
     return `🚀 ${capitalize(item.payload.action)} release ${toUrlFormat(
-      item.payload.release.name
-        ? item.payload.release.name
-        : item.payload.release.tag_name
+      item
     )} in ${toUrlFormat(item.repo.name)}`;
   },
 };
