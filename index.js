a32ab28a9e4fc5423a4e5fd2713c7627b2ed621e
Ratiq Narwal <ratiqnarwal@gmail.com>
2025-12-31 19:15:47 -0800

    add debug statement

--- index.js
+++ index.js
@@ -207,6 +207,8 @@ Toolkit.run(
       .slice(0, MAX_LINES)
       // Call the serializer to construct a string
       .map((item) => serializers[item.type](item));
+	
+	tools.log.debug(content)
 
     const readmeContent = fs
       .readFileSync(`./${TARGET_FILE}`, "utf-8")
